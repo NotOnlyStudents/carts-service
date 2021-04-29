5fbe1a12dc08e95449e6d0aeb608605101a0f2e5
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-04-29 13:35:58 +0200

    Dynamodb table refactor

--- src/models/DynamoDbCartProduct.ts
+++ src/models/DynamoDbCartProduct.ts
@@ -1,13 +1,14 @@
 import {
   attribute,
   hashKey,
+  rangeKey,
   table,
 } from '@aws/dynamodb-data-mapper-annotations';
 import Product from './interfaces/Product';
 
-@table(process.env.PRODUCTS_TABLE)
-class DynamoDbProduct implements Product {
-  @hashKey()
+@table(process.env.CARTS_TABLE)
+class DynamoDbCartProduct implements Product {
+  @rangeKey()
   id: string;
 
   @attribute()
@@ -34,8 +35,12 @@ class DynamoDbProduct implements Product {
   @attribute()
   images: string[];
 
-  constructor(id = '', name = '', description = '', price = 0.0, quantity = 1, available = true, evidence = false, categories = [], images = []) {
+  @hashKey()
+  cartId: string;
+
+  constructor(id = '', cartId = '', name = '', description = '', price = 0.0, quantity = 1, available = true, evidence = false, categories = [], images = []) {
     this.id = id;
+    this.cartId = cartId;
     this.name = name;
     this.description = description;
     this.price = price;
@@ -47,4 +52,4 @@ class DynamoDbProduct implements Product {
   }
 }
 
-export default DynamoDbProduct;
+export default DynamoDbCartProduct;
--- src/repository/DynamoDbCartRepository.ts
+++ src/repository/DynamoDbCartRepository.ts
@@ -2,10 +2,11 @@ import { DataMapper } from '@aws/dynamodb-data-mapper';
 import { DynamoDB } from 'aws-sdk';
 import Product from '../models/interfaces/Product';
 import Cart from '../models/interfaces/Cart';
-import DynamoDbCart from '../models/DynamoDbCart';
 import CartRepositoryGet from './interfaces/CartRepositoryGet';
 import CartRepositoryPost from './interfaces/CartRepositoryPost';
 import CartRepositoryPatch from './interfaces/CartRepositoryPatch';
+import DynamoDbCartProduct from 'src/models/DynamoDbCartProduct';
+import RealCart from '../models/RealCart';
 
 class DynamoDbCartRepository implements CartRepositoryGet, CartRepositoryPost, CartRepositoryPatch {
   private mapper: DataMapper;
@@ -14,13 +15,54 @@ class DynamoDbCartRepository implements CartRepositoryGet, CartRepositoryPost, C
     this.mapper = new DataMapper({ client: dynamodb });
   }
 
-  getCart = async (id: string): Promise<Cart> => this.mapper.get(new DynamoDbCart(id));
+  getCart = async (id: string): Promise<Cart> => {
+    const asyncIterator = this.mapper.query(DynamoDbCartProduct, { cartId: id });
+    const cart = new RealCart(id);
+    for await (const product of asyncIterator) {
+      cart.products.push(product);
+    }
+    return cart;
+  }
 
-  createCart = async (cart: Cart): Promise<Cart> => this.mapper.put(cart);
+  createCart = async (cart: Cart): Promise<Cart> => {
+    cart.products.forEach(product => (
+      this.mapper.put(
+        new DynamoDbCartProduct(
+          product.id, 
+          cart.id, 
+          product.name, 
+          product.description, 
+          product.price, 
+          product.quantity, 
+          product.available, 
+          product.evidence, 
+          product.categories, 
+          product.images
+        )
+      )
+    ));
+    return cart;
+  }
 
   updateCart = async (id: string, products: Product[]): Promise<Cart> => {
-    const cart = new DynamoDbCart(id, products);
-    return this.mapper.update(cart, { onMissing: 'skip' });
+    products.forEach(product => (
+      this.mapper.update(
+        new DynamoDbCartProduct(
+          product.id,
+          id,
+          product.name,
+          product.description,
+          product.price,
+          product.quantity,
+          product.available,
+          product.evidence,
+          product.categories,
+          product.images
+        )
+      )
+    ))
+
+    return new RealCart(id, products);
   };
 }
 
